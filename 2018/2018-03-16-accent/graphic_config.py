9328e9d20cc2b0ebf2dabc8739b7774bd39aecfe
Petrin <@stlpublicradio.org>
2018-08-30 17:18:54 -0500

    archives Petrin graphics through 2018-05-24

---
+++ 2018/2018-03-16-accent/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1jD1ZOLJ6mpk9b6ZJASRd9_DhPfHCehk87P_ts-hieUE'
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
---
+++ 2018/2018-04-23-greitens-judge/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1pYmTdY3MB9VnmA9VQi8_yf-D7GRb8Nm-8_ry331wfRc'
+
+USE_ASSETS = True
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
