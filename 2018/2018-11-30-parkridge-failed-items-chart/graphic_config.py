d642edb3f11ba513cd08355ec2bf39734b506dd2
Petrin <@stlpublicradio.org>
2019-01-04 16:41:49 -0600

    archives KMP graphics for December

---
+++ 2018/2018-11-30-parkridge-failed-items-chart/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1_FXak7H0Oy2WAAojQT88EF9E2-dG7Jv_pv9w4xv-330'
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
