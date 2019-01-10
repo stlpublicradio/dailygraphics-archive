36b9f81571ff99113d670f17396182c218d59f90
kmaepetrin <kpetrin@stlpublicradio.org>
2019-01-10 14:59:25 -0600

    adds november graphics by Petrin

---
+++ 2018/2018-11-01-mo-auditor-election/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1FWyfF6cybVBDKYxG25h80PNgnZi7DzWvjt5KX3getmU'
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
